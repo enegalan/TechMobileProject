babff7c3fe5bfd100366b11f5a3edc2fd87605d9
= <=>
2024-01-11 18:49:01 +0100

    Environment variables added in order to use a .env

--- php/conn.php
+++ php/conn.php
@@ -1,5 +1,14 @@
-<?php 
-$conn = mysqli_connect("localhost","id21343237_egalan","Techmobile1!","techmobile");
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+
+try {
+    $dotenv->load();
+} catch (\Throwable $e) {
+    echo "Error loading environment variables: " . $e->getMessage();
+}
+
+$conn = mysqli_connect(getenv('DB_HOST'),getenv('DB_USER'),getenv('DB_PASSWORD'),getenv('DB_SCHEMA'));
 if(!$conn){
     echo "Error database connection";
     die;
